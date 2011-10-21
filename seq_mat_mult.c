66915777d34b6fe3444bfe65cc23d1e0994f7c0d
Sandeep Raghuraman <sandy.8925@gmail.com>
2011-10-21 18:26:44 -0400

    seq_mat_mult.c: initialized matrices with random numbers instead of zero

--- seq_mat_mult.c
+++ seq_mat_mult.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <strings.h>
 
 #define mat_comm_dim 1000
-
 #define matA_rows 1000
 #define matA_cols mat_comm_dim
 #define matB_rows mat_comm_dim
@@ -13,28 +13,26 @@
 
 int main()
 {
-
-int matrixA[matA_rows][matA_cols];
-int matrixB[matB_rows][matB_cols];
-int matrixC[matC_rows][matC_cols];
+int *matrixA,*matrixB,*matrixC;
+matrixA = (int *)calloc(sizeof(int),matA_rows*matA_cols);
+matrixB = (int *)calloc(sizeof(int),matB_rows*matB_cols);
+matrixC = (int *)calloc(sizeof(int),matC_rows*matC_cols);
 int temp,counter1,counter2,counter3;
 
 //TODO: read input matrices from file
 
-//initialize matrix A - zeros for now
-//TODO: initialize with random numbers
-for(counter1=0; counter1 < matA_rows; counter1++)
+//initialize matrix A with random numbers
+for(counter1=0 ; counter1 < matA_rows ; counter1++)
 {
-  for(counter2=0; counter2 < matA_cols; counter2++)
-  { matrixA[counter1][counter2] = 0; }
+ for(counter2=0; counter2 < matA_cols; counter2++) 
+ { *(matrixA + counter1*matA_cols + counter2) = (int) random()%65536; }
 }
 
-//initialize matrix B - zeros for now
-//TODO: initialize with random numbers
-for(counter1=0; counter1 < matB_rows; counter1++)
+//initialize matrix B with random numbers
+for(counter1=0 ; counter1 < matB_rows ; counter1++)
 {
-  for(counter2=0; counter2 < matB_cols; counter2++)
-  { matrixB[counter1][counter2] = 0; }
+ for(counter2=0; counter2 < matB_cols; counter2++) 
+ { *(matrixB + counter1*matB_cols + counter2) = (int) random()%65536; }
 }
 
 //do matrix multiplication
@@ -44,12 +42,16 @@ for(counter1=0; counter1 < matC_rows; counter1++)
   {
     temp = 0;
     for(counter3=0; counter3 < mat_comm_dim; counter3++)
-    { temp = temp + matrixA[counter1][counter3]*matrixB[counter3][counter2]; }
-    matrixC[counter1][counter2] = temp;
+    { temp = temp + *(matrixA + counter1*matA_cols + counter3) * *(matrixB + counter3*matB_cols + counter2); }
+    *(matrixC + counter1*matC_cols + counter2) = temp;
   }
 }
 
 //TODO: write results to file
 
+//free allocated space
+free(matrixA);
+free(matrixB);
+free(matrixC);
 
 }
---
+++ test_programs/test3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("sizeof(long) = %u\n",sizeof(long));
+return 0;
+}
