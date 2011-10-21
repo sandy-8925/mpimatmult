d36f5f7cd45e100ac9911cac4048e84187a7d7c0
Sandeep Raghuraman <sandy.8925@gmail.com>
2011-10-20 22:29:45 -0400

    first project version

---
+++ seq_mat_mult.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+#define mat_comm_dim 1000
+
+#define matA_rows 1000
+#define matA_cols mat_comm_dim
+#define matB_rows mat_comm_dim
+#define matB_cols 1000
+#define matC_rows matA_rows
+#define matC_cols matB_cols
+
+
+int main()
+{
+
+int matrixA[matA_rows][matA_cols];
+int matrixB[matB_rows][matB_cols];
+int matrixC[matC_rows][matC_cols];
+int temp,counter1,counter2,counter3;
+
+//TODO: read input matrices from file
+
+//initialize matrix A - zeros for now
+//TODO: initialize with random numbers
+for(counter1=0; counter1 < matA_rows; counter1++)
+{
+  for(counter2=0; counter2 < matA_cols; counter2++)
+  { matrixA[counter1][counter2] = 0; }
+}
+
+//initialize matrix B - zeros for now
+//TODO: initialize with random numbers
+for(counter1=0; counter1 < matB_rows; counter1++)
+{
+  for(counter2=0; counter2 < matB_cols; counter2++)
+  { matrixB[counter1][counter2] = 0; }
+}
+
+//do matrix multiplication
+for(counter1=0; counter1 < matC_rows; counter1++)
+{
+  for(counter2=0; counter2 < matC_cols; counter2++)
+  {
+    temp = 0;
+    for(counter3=0; counter3 < mat_comm_dim; counter3++)
+    { temp = temp + matrixA[counter1][counter3]*matrixB[counter3][counter2]; }
+    matrixC[counter1][counter2] = temp;
+  }
+}
+
+//TODO: write results to file
+
+
+}
---
+++ test_programs/test2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+printf("sizeof(int) = %u\n",sizeof(int));
+return 0;
+}
