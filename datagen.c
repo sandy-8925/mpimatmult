fc6137a535708f77bb461e623e605d5fc1d07770
Sandeep Raghuraman <sandy.8925@gmail.com>
2011-10-28 16:23:19 -0400

    Data generation program created

---
+++ datagen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+//TODO: accept these variables as commandline arguments
+
+int mat_comm_dim = 1000;
+int matA_rows = 1000,matA_cols = mat_comm_dim,matB_rows = mat_comm_dim,matB_cols = 1000;
+
+int *matrixA,*matrixB;
+int counter1,counter2;
+
+matrixA = (int *)calloc(sizeof(int),matA_rows*matA_cols);
+matrixB = (int *)calloc(sizeof(int),matB_rows*matB_cols);
+
+//initialize matrix A with random numbers
+for(counter1=0 ; counter1 < matA_rows ; counter1++)
+{
+ for(counter2=0; counter2 < matA_cols; counter2++) 
+ { *(matrixA + counter1*matA_cols + counter2) = (int) random()%65536; }
+}
+
+//initialize matrix B with random numbers
+for(counter1=0 ; counter1 < matB_rows ; counter1++)
+{
+ for(counter2=0; counter2 < matB_cols; counter2++) 
+ { *(matrixB + counter1*matB_cols + counter2) = (int) random()%65536; }
+}
+
+//TODO: write matrices to file
+
+
+return 0;
+}
