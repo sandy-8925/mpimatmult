261e3a56b7999827db96022602c2da893ff3f8f7
Sandeep Raghuraman <sandy.8925@gmail.com>
2011-11-05 19:22:41 -0400

    First MPI program - Hello world in MPI

---
+++ test_programs/mpi1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int argc,char **argv)
+{
+int ierr, myid, numprocs;
+
+MPI_Init(&argc,&argv);
+
+MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+printf("Hello world, I am process %d of %d\n", myid, numprocs);
+
+MPI_Finalize();
+}
