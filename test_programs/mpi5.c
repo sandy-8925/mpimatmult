6c51842a89c0858017500506541b2fc633d23429
Sandeep Raghuraman <sandy.8925@gmail.com>
2011-11-08 16:40:25 -0500

    Modified mpi5.c to have main program send matrix to all other nodes

--- test_programs/mpi5.c
+++ test_programs/mpi5.c
@@ -7,6 +7,7 @@ int i,ierr,rank,size,dest,source,from,to,count,tag;
 int stat_count, stat_source, stat_tag;
 int mat1_rows=1000,mat1_cols=1000;
 int *matrix1;
+int matsize = mat1_rows * mat1_cols;
 MPI_Status status;
 
 MPI_Init(&argc,&argv);
@@ -14,26 +15,22 @@ MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 printf("I am process %d of %d\n", rank, size);
-dest = size - 1;
 source = 0;
 
 if( rank == source )
 {
-to = dest;
-count = SIZE;
 tag = 11;
-matrix1 = (int *) calloc(mat1_rows*mat1_cols, sizeof(int));
-for(i=0 ; i<SIZE ; i++) { *(data + i) = i; }
+matrix1 = (int *) calloc(matsize, sizeof(int));
+for(i=0 ; i<matsize ; i++) { *(matrix1 + i) = i%255; }
 for(to=1 ; to < size ; to++)
-    ierr = MPI_Send(data, count, MPI_REAL, to, tag, MPI_COMM_WORLD);
+    ierr = MPI_Send(matrix1, matsize, MPI_INT, to, tag, MPI_COMM_WORLD);
 }
 else
 {
 tag = MPI_ANY_TAG;
-count = SIZE;
 from = MPI_ANY_SOURCE;
-matrix1 = (int *) calloc(SIZE, sizeof(int));
-ierr = MPI_Recv(data, count, MPI_REAL, from, tag, MPI_COMM_WORLD, &status);
+matrix1 = (int *) calloc(matsize, sizeof(int));
+ierr = MPI_Recv(matrix1, matsize, MPI_INT, from, tag, MPI_COMM_WORLD, &status);
 ierr = MPI_Get_count(&status, MPI_REAL, &stat_count);
 stat_source = status.MPI_SOURCE;
 stat_tag = status.MPI_TAG;
