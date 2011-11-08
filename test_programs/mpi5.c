0683a59a678e766ce84000580eef3efc1c39d7f7
Sandeep Raghuraman <sraghura@buffalo.edu>
2011-11-08 16:18:45 -0500

    Modified and added MPI examp

--- test_programs/mpi5.c
+++ test_programs/mpi5.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <mpi.h>
-#include <stdlib.h>
-
-#define SIZE 10000
 
 int main(int argc, char **argv)
 {
 int i,ierr,rank,size,dest,source,from,to,count,tag;
 int stat_count, stat_source, stat_tag;
-float *data;
+int mat1_rows=1000,mat1_cols=1000;
+int *matrix1;
 MPI_Status status;
 
 MPI_Init(&argc,&argv);
@@ -24,7 +22,7 @@ if( rank == source )
 to = dest;
 count = SIZE;
 tag = 11;
-data = (float *) calloc(SIZE, sizeof(float));
+matrix1 = (int *) calloc(mat1_rows*mat1_cols, sizeof(int));
 for(i=0 ; i<SIZE ; i++) { *(data + i) = i; }
 for(to=1 ; to < size ; to++)
     ierr = MPI_Send(data, count, MPI_REAL, to, tag, MPI_COMM_WORLD);
@@ -34,7 +32,7 @@ else
 tag = MPI_ANY_TAG;
 count = SIZE;
 from = MPI_ANY_SOURCE;
-data = (float *) calloc(SIZE, sizeof(float));
+matrix1 = (int *) calloc(SIZE, sizeof(int));
 ierr = MPI_Recv(data, count, MPI_REAL, from, tag, MPI_COMM_WORLD, &status);
 ierr = MPI_Get_count(&status, MPI_REAL, &stat_count);
 stat_source = status.MPI_SOURCE;
@@ -42,7 +40,7 @@ stat_tag = status.MPI_TAG;
 printf("Process %d: Status of receive: dest=%d source=%d tag=%d count=%d\n", rank, rank, stat_source, stat_tag, stat_count);
 }
 
-free(data);
+
 ierr = MPI_Finalize();
 return 0;
 }
