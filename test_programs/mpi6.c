60403beff76310585e3177298ed74e76f1713f9e
Sandeep Raghuraman <sraghura@buffalo.edu>
2011-11-13 18:35:12 -0500

    Used MPI broadcast to broadcast data instead of using normal send/receive

--- test_programs/mpi6.c
+++ test_programs/mpi6.c
@@ -4,13 +4,13 @@
 
 int main(int argc, char **argv)
 {
-int i,ierr,rank,size,dest,source,from,to,count,tag;
+int ierr,rank,size,dest,source,from,to,count,tag;
+long i;
 int stat_count, stat_source, stat_tag;
 int mat1_rows=10000,mat1_cols=10000;
 int *matrix1;
-int matsize = mat1_rows * mat1_cols;
+long matsize = mat1_rows * mat1_cols;
 MPI_Status status;
-MPI_Request request;
 
 MPI_Init(&argc,&argv);
 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -19,28 +19,19 @@ MPI_Comm_size(MPI_COMM_WORLD, &size);
 printf("I am process %d of %d\n", rank, size);
 source = 0;
 
+matrix1 = (int *) calloc(matsize, sizeof(int));
+
 if( rank == source )
 {
-tag = 11;
-matrix1 = (int *) calloc(matsize, sizeof(int));
 for(i=0 ; i<matsize ; i++) { *(matrix1 + i) = i%255; }
-for(to=1 ; to < size ; to++)
-    ierr = MPI_Isend(matrix1, matsize, MPI_INT, to, tag, MPI_COMM_WORLD,&request);
-printf("Process %d: Finished sending all data\n",rank);
+MPI_Bcast(matrix1, matsize, MPI_INT, source, MPI_COMM_WORLD);
 }
 else
 {
-tag = MPI_ANY_TAG;
-from = MPI_ANY_SOURCE;
-matrix1 = (int *) calloc(matsize, sizeof(int));
-ierr = MPI_Recv(matrix1, matsize, MPI_INT, from, tag, MPI_COMM_WORLD, &status);
-ierr = MPI_Get_count(&status, MPI_INT, &stat_count);
-stat_source = status.MPI_SOURCE;
-stat_tag = status.MPI_TAG;
-printf("Process %d: Status of receive: dest=%d source=%d tag=%d count=%d\n", rank, rank, stat_source, stat_tag, stat_count);
+MPI_Bcast(matrix1, matsize, MPI_INT, source, MPI_COMM_WORLD);
 }
-
-
+printf("Process %d finished\n",rank);
+free(matrix1);
 ierr = MPI_Finalize();
 return 0;
 }
