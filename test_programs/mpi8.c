66604a60f2b739b4b248d16cb56333460df961dd
Sandeep Raghuraman <sraghura@buffalo.edu>
2011-11-15 12:29:18 -0500

    Increased matrix size and output string size in mpi8.c

--- test_programs/mpi8.c
+++ test_programs/mpi8.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 int ierr,rank,size,dest,source,from,to,count,tag;
 long i,counter;
 int stat_count, stat_source, stat_tag;
-int mat1_rows=10,mat1_cols=10;
+int mat1_rows=100,mat1_cols=100;
 int *matrix1,*matrix2;
 long matsize = mat1_rows * mat1_cols;
 MPI_Status status;
@@ -30,22 +30,20 @@ matrix2 = (int *) calloc(matsize/size, sizeof(int));
 if( rank == source )
 {
 //initialize all data
-for(i=0 ; i<matsize ; i++) { *(matrix1 + i) = i%255; }
+for(i=0 ; i<matsize ; i++) { *(matrix1 + i) = i; }
 }
 
 MPI_Scatter(matrix1, matsize/size, MPI_INT, matrix2, matsize/size, MPI_INT, source, MPI_COMM_WORLD);
-
-char *str1,*temp;
+char str1[9000],temp[15];
 sprintf(str1,"Process %d data:\n",rank);
 for(counter=0;counter<matsize/size;counter++)
 {
 sprintf(temp,"%d ",*(matrix2 + counter));
 strcat(str1,temp);
 }
-printf("%s",str1);
+printf("%s\n",str1);
 
 printf("Process %d finished\n",rank);
-
 
 ierr = MPI_Finalize();
 free(matrix1);
